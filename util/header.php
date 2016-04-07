dd48de7e09e4cfb2e0c7f7116b3c4bff1d9fe575
Eric <ericpatel3000@gmail.com>
2016-04-06 22:13:49 -0400

    stuff

--- util/header.php
+++ util/header.php
@@ -1,5 +1,41 @@
 <head>
-    <link rel="stylesheet" href="../style_sheets/semantic.css">
-    <link rel="stylesheet" href="../style_sheets/style.css">
-    <script type="text/javascript" src="../style_sheets/script.js"></script>
+    <link rel="stylesheet" href="style_sheets/semantic.css">
+    <link rel="stylesheet" href="style_sheets/style.css">
+
+    <script type="text/javascript" src="style_sheets/semantic.js"></script>
+    
+    <!--- Site CSS -->
+    <link rel="stylesheet" type="text/css" href="style_sheets/dist/components/reset.css">
+    <link rel="stylesheet" type="text/css" href="style_sheets/dist/components/site.css">
+    <link rel="stylesheet" type="text/css" href="style_sheets/dist/components/grid.css">
+
+    <!--- Component CSS -->
+    <link rel="stylesheet" type="text/css" href="style_sheets/dist/components/icon.css">
+    <link rel="stylesheet" type="text/css" href="style_sheets/dist/components/input.css">
+    <link rel="stylesheet" type="text/css" href="style_sheets/dist/components/button.css">
+    <link rel="stylesheet" type="text/css" href="style_sheets/dist/components/divider.css">
+    <link rel="stylesheet" type="text/css" href="style_sheets/dist/components/label.css">
+    <link rel="stylesheet" type="text/css" href="style_sheets/dist/components/dropdown.css">
+    <link rel="stylesheet" type="text/css" href="style_sheets/dist/components/transition.css">
+    <link rel="stylesheet" type="text/css" href="style_sheets/dist/components/popup.css">
+
+    <!--- Component JS -->
+    <script src="style_sheets/assets/library/jquery.min.js"></script>
+    <script src="style_sheets/assets/library/iframe-content.js"></script>
+    <script type="text/javascript" src="style_sheets/dist/components/popup.js"></script>
+    <script type="text/javascript" src="style_sheets/dist/components/dropdown.js"></script>
+    <script type="text/javascript" src="style_sheets/dist/components/transition.js"></script>
+
+    <!--- Example Javascript -->
+    <script>
+        $(document)
+            .ready(function() {
+                $('.ui.dropdown')
+                    .dropdown({
+                        on: 'click'
+                    })
+                ;
+            })
+        ;
+    </script>
 </head>
